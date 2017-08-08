91a24540264a1c7b05a9a0ff63e24728455a894b
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-08 13:37:39 -0400

    submit button accesses api

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("talking");
+
+    let submit = document.getElementById('submit');
+    submit.addEventListener('click', search, false);
+
+    function search(){
+      fetch('http://recipepuppyproxy.herokuapp.com/api/?i=onions,garlic&q=omelet&p=3')
+      //www.recipepuppy.com/api/?q=search-term
+
+        .then(
+          function(response) {
+            if (response.status !== 200) {
+              console.log('Looks like there was a problem. Status Code: ' +
+                response.status);
+              return;
+            }
+
+            response.json().then(function(data) {
+              console.log(data);
+
+
+            });
+          }
+        )
+}
