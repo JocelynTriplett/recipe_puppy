2a4a3c8308831fef7214a3d23c3d7d4953f0f786
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-08 15:35:14 -0400

    chooses thumbnail or placeholder

--- main.js
+++ main.js
@@ -20,7 +20,38 @@ console.log("talking");
 
             response.json().then(function(data) {
               console.log(data);
+              let row1 = document.getElementsByClassName('row1')[0];
+              let row2 = document.getElementsByClassName('row2')[0];
+              row1.innerHTML = '<div></div>' // clears previous search results
+              row2.innerHTML = '<div></div>'
+              for (var i = 0; i < 8; i++) {
+                let title = data.results[i].title;
+                let href = data.results[i].href;
+                var thumbnail = data.results[i].thumbnail;
+                if (thumbnail == "") {
+                  thumbnail = "http://via.placeholder.com/200x200";
+                }
 
+                let recipe = document.createElement("a");
+
+                recipe.setAttribute("href", href);
+                let recipeInfo = `
+
+                <div class="flex-item">
+                  <img src="${thumbnail}" />
+                  <p>${title}</p>
+                </div>
+
+                `
+                if (i<4){
+                row1.appendChild(recipe);
+                recipe.innerHTML = recipeInfo;
+                }
+                else {
+                  row2.appendChild(recipe);
+                  recipe.innerHTML = recipeInfo;
+                }
+              }
 
             });
           }
